2493ffa968024b19a62925b30474f2fa8801f36a
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2024-07-22 00:26:48 -0400

    feat: Standardize suspect names

--- src/sets.ts
+++ src/sets.ts
@@ -1,8 +1,11 @@
 import type { GameSet } from './types';
 
+/** Default list of suspects */
+const suspects = () => ['Green', 'Plum', 'Mustard', 'Peacock', 'Scarlett', 'White'];
+
 const SETS = {
     Clue: {
-        suspects: ['Mustard', 'White', 'Scarlett', 'Peacock', 'Plum', 'Green'],
+        suspects: suspects(),
         weapons: ['Candlestick', 'Dagger', 'Lead Pipe', 'Revolver', 'Rope', 'Wrench'],
         rooms: [
             'Ballroom',
@@ -17,7 +20,7 @@ const SETS = {
         ],
     },
     'Murder Express': {
-        suspects: ['Scarlett', 'Peacock', 'Orchid', 'Green', 'Mustard', 'Plum'],
+        suspects: suspects(),
         weapons: ['Lamp', 'Knife', 'Coal Shovel', 'Glass', 'Velvet Rope', 'Spanner'],
         rooms: [
             'Signal Box',
@@ -32,7 +35,7 @@ const SETS = {
         ],
     },
     'Tropical Mystery': {
-        suspects: ['Mustard', 'White', 'Brunette', 'Plum', 'Peach', 'Scarlett'],
+        suspects: suspects(),
         weapons: ['Harpoon', 'Diving Knife', 'Oxygen Tank', 'Flare Gun', 'Spyglass', 'Oar'],
         rooms: [
             'Helicopter Pad',
@@ -47,7 +50,7 @@ const SETS = {
         ],
     },
     Hollywood: {
-        suspects: ['Mustard', 'White', 'Moriarty', 'Green', 'Peach', 'Rose'],
+        suspects: suspects(),
         weapons: ['Award', 'Dagger', 'Lead Pipe', 'Pistol', 'Velvet Rope', 'Wrench'],
         rooms: [
             'Roman Set',
