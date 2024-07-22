9387816bb40bc60969fc8284f1da3fae0ad2df1e
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2024-07-22 00:20:07 -0400

    feat: Add option to reload default sets

--- src/lib/SetSelector.svelte
+++ src/lib/SetSelector.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
     import { Panel, Header, Content } from '@smui-extra/accordion';
     import Button, { Label, Icon } from '@smui/button';
+    import IconButton from '@smui/icon-button';
     import Select, { Option } from '@smui/select';
+    import Tooltip, { Wrapper } from '@smui/tooltip';
 
     import SetManager from './SetManager.svelte';
 
     import SETS from '../sets';
     import { customSets, getSet, set } from '../stores';
-    import IconButton from '@smui/icon-button';
 
     let setName = $set[0];
     $: $set = [setName, getSet(setName)];
@@ -20,6 +21,15 @@
         setName = Object.keys($customSets)[0];
         $set = [setName, getSet(setName)];
         $customSets = $customSets;
+    }
+
+    function restore() {
+        for (const builtin of Object.keys(SETS) as Array<keyof typeof SETS>) {
+            $customSets[builtin] = structuredClone(SETS[builtin]);
+        }
+
+        $customSets = $customSets;
+        $set = [$set[0], getSet($set[0])];
     }
 </script>
 
@@ -53,5 +63,12 @@
             <Label>Create New Set</Label>
             <Icon class="material-icons">add</Icon>
         </Button>
+        <Wrapper>
+            <Button on:click={restore}>
+                <Label>Reload Builtin Sets</Label>
+                <Icon class="material-icons">restart_alt</Icon>
+            </Button>
+            <Tooltip>Restore the builtin sets to their default values.</Tooltip>
+        </Wrapper>
     </Content>
 </Panel>
