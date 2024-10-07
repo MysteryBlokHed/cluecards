36732d2381364523d805662d78aab9aa279db369
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2024-10-07 15:56:11 -0400

    feat: Multiple icon sizes

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import 'core-js/actual/set/union';
 import './app.css';
 import App from './App.svelte';
 
+// Include icons in output
+import '../images/icon-192.png';
+import '../images/icon-512.png';
+
 const app = new App({
     target: document.getElementById('app')!,
 });
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [svelte()],
+    build: {
+        rollupOptions: {
+            output: {
+                assetFileNames(chunkInfo) {
+                    if (chunkInfo.originalFileName?.includes('images/'))
+                        return chunkInfo.originalFileName;
+                    return chunkInfo.name || '';
+                },
+            },
+        },
+    },
 });
