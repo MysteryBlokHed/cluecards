2696b838682a82baee1af292ba6d71a404f10cac
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2024-10-22 11:31:56 -0400

    perf(worker): Delete cache entry when an asset 404's
    Should be the simplest + fastest way to detect when there's a new
    version of a cached asset.

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,14 @@
-async function cacheFirstWithRefresh(request) {
+/// <reference lib="webworker" />
+
+async function cacheFirstWithRefresh(/** @type {Request} */ request) {
     const fetchResponsePromise = fetch(request).then(async networkResponse => {
         if (networkResponse.ok) {
             const cache = await caches.open('Cluecards');
             cache.put(request, networkResponse.clone());
+        } else if (networkResponse.status === 404 && request.url.includes('/assets/')) {
+            // Assume that the resource we're looking for was an old version of a dynamic asset, and remove it from the cache
+            const cache = await caches.open('Cluecards');
+            cache.delete(request);
         }
         return networkResponse;
     });
@@ -10,6 +16,6 @@ async function cacheFirstWithRefresh(request) {
     return (await caches.match(request)) || (await fetchResponsePromise);
 }
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (/** @type {FetchEvent} */ event) => {
     event.respondWith(cacheFirstWithRefresh(event.request));
 });
