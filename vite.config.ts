1913dea6714b435bf8b4d7f3ef4d68a813507da9
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2024-12-28 18:36:12 -0500

    build: Remove unused build code

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,5 @@ import wasm from 'vite-plugin-wasm';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [svelte(), wasm()],
-    build: {
-        target: 'es2022',
-        rollupOptions: {
-            output: {
-                assetFileNames(chunkInfo) {
-                    // eslint-disable-next-line
-                    // @ts-ignore
-                    if (chunkInfo.originalFileName?.includes('images/'))
-                        return chunkInfo.originalFileName;
-                    return chunkInfo.name || '';
-                },
-            },
-        },
-    },
+    build: { target: 'es2022' },
 });
